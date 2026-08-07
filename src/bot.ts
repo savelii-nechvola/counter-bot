6dce21424ff505974fe71d0d7d9f15124aa8aef2
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-08-08 02:56:58 +0300

    feat: create and list tags

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,13 @@
 import { type Api, Bot, type Context, type PollingOptions } from "grammy";
+import {
+  createTag,
+  getTagByChatIdAndName,
+  listTags,
+  type Tag,
+  transaction,
+} from "./db.js";
 import { envVars } from "./env.js";
+import { err, ok, type Result } from "./utils.js";
 
 type AppBot = Bot<Context, Api>;
 
@@ -23,6 +31,8 @@ function registerCommands(bot: AppBot): void {
   startCommand(bot);
   echoCommand(bot);
   randomizeTagCommand(bot);
+  listCommand(bot);
+  createTagCommand(bot);
 }
 
 function startCommand(bot: AppBot): void {
@@ -30,12 +40,50 @@ function startCommand(bot: AppBot): void {
 }
 
 function echoCommand(bot: AppBot): void {
-  bot.command("echo", (ctx) => ctx.reply(ctx.message?.text ?? ""));
+  bot
+    .chatType(["group", "supergroup"])
+    .command("echo", (ctx) => ctx.reply(ctx.message.text ?? ""));
 }
 
 function randomizeTagCommand(bot: AppBot): void {
   bot.chatType(["group", "supergroup"]).command("randomize", async (ctx) => {
     const random = String(Math.random());
     await ctx.reply(random);
+  });
+}
+
+function createTagCommand(bot: AppBot): void {
+  bot.chatType(["group", "supergroup"]).command("newtag", async (ctx) => {
+    const name = ctx.message.text.split(" ")[1];
+    if (!name) {
+      await ctx.reply("Provide a tag name as a command argument");
+      return;
+    }
+    if (name.length > 50) {
+      await ctx.reply("Tag name should be <= 50 chars");
+      return;
+    }
+
+    const res = transaction((): Result<Tag, string> => {
+      const existingTag = getTagByChatIdAndName(ctx.chatId, name);
+      if (existingTag) {
+        return err("Tag with this name already exists");
+      }
+      return ok(createTag({ chatId: ctx.chatId, name }));
+    });
+
+    if (!res.ok) {
+      await ctx.reply(res.error);
+      return;
+    }
+
+    await ctx.reply(JSON.stringify(res.value));
+  });
+}
+
+function listCommand(bot: AppBot): void {
+  bot.chatType(["group", "supergroup"]).command("listtags", async (ctx) => {
+    const tags = listTags(ctx.chatId);
+    await ctx.reply(JSON.stringify(tags));
   });
 }
--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,51 @@
 import { DatabaseSync } from "node:sqlite";
 
 export const db = new DatabaseSync("db.db");
+
+export function transaction<T>(fn: () => T): T {
+  db.exec("begin");
+  try {
+    const res = fn();
+    db.exec("commit");
+    return res;
+  } catch (e) {
+    db.exec("rollback");
+    throw e;
+  }
+}
+
+type NewTag = {
+  chatId: number;
+  name: string;
+};
+
+export type Tag = {
+  id: number;
+  chatId: number;
+  name: string;
+};
+
+export function getTagByChatIdAndName(
+  chatId: number,
+  name: string,
+): Tag | null {
+  return (db
+    .prepare(`select id, chat_id, name from tag where chat_id = ? and name = ?`)
+    .get(chatId, name) ?? null) as Tag | null;
+}
+
+export function createTag(input: NewTag): Tag {
+  return db
+    .prepare(`
+      insert into tag (chat_id, name)
+      values (?, ?)
+      returning id, chat_id, name
+    `)
+    .get(input.chatId, input.name) as Tag;
+}
+
+export function listTags(chatId: number): Tag[] {
+  return db
+    .prepare(`select id, chat_id, name from tag where chat_id = ?`)
+    .all(chatId) as Tag[];
+}
