71cf73c50e3445562fa3ba9ba9622650cbeb6101
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-08-08 01:48:53 +0300

    feat: bot commands

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,41 @@
-import { Bot, type PollingOptions } from "grammy";
+import { type Api, Bot, type Context, type PollingOptions } from "grammy";
 import { envVars } from "./env.js";
+
+type AppBot = Bot<Context, Api>;
 
 export function startBot(options?: PollingOptions): void {
   const bot = new Bot(envVars.BOT_TOKEN);
-  bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-  bot.on("message", (ctx) => ctx.reply("Got another message!"));
-  bot.catch((handler) => console.error(handler));
+  registerCommands(bot);
+  bot.catch((error) =>
+    console.error(
+      error.message,
+      error.error,
+      error.name,
+      error.cause,
+      error.stack,
+    ),
+  );
 
   void bot.start(options);
+}
+
+function registerCommands(bot: AppBot): void {
+  startCommand(bot);
+  testCommand(bot);
+  randomizeTagCommand(bot);
+}
+
+function startCommand(bot: AppBot): void {
+  bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+}
+
+function testCommand(bot: AppBot): void {
+  bot.command("test", (ctx) => ctx.reply(ctx.message?.text ?? ""));
+}
+
+function randomizeTagCommand(bot: AppBot): void {
+  bot.chatType(["group", "supergroup"]).command("randomize", async (ctx) => {
+    const random = String(Math.random());
+    await ctx.reply(random);
+  });
 }
