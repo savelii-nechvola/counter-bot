f3b328286a068ae467967e578d5bd71307c38dc2
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-08-08 03:16:57 +0300

    feat: update env

--- src/env.ts
+++ src/env.ts
@@ -1,24 +1,14 @@
 type Env = {
   BOT_TOKEN: string;
-  MEMBER_TAG: string;
 };
 
 function getEnv(): Env {
   const BOT_TOKEN = (process.env.BOT_TOKEN ?? "").trim();
-  const MEMBER_TAG = (process.env.MEMBER_TAG ?? "").trim();
 
   const errors: { [K in keyof Env]?: string } = {};
 
   if (!BOT_TOKEN) {
     errors.BOT_TOKEN = "BOT_TOKEN is not provided";
-  }
-
-  if (!MEMBER_TAG) {
-    errors.MEMBER_TAG = "MEMBER_TAG is not provided";
-  }
-
-  if (MEMBER_TAG.length > 8) {
-    errors.MEMBER_TAG = "must be at most 8 characters long";
   }
 
   if (Object.keys(errors).length) {
@@ -27,7 +17,6 @@ function getEnv(): Env {
 
   return {
     BOT_TOKEN: BOT_TOKEN,
-    MEMBER_TAG: MEMBER_TAG,
   };
 }
 
